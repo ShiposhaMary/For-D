b3ef2803205f5c72d61db9509e01247094a4842b
ShiposhaMary <rusya@mail.ru>
2020-06-24 16:58:16 +0300

    writing in bin

--- For D/Program.cs
+++ For D/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,9 +17,40 @@ namespace For_D
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int sizeRes = 4;
+            int sr = sizeRes;
+            Matrix.bfBuffer = new double[7, 103];
+           // Matrix.resBuffer = new Matrix(1600, 1103);
+            string[] source = Directory.GetFiles(@"C:\Work\duble\");
+            foreach (var file in source)
+            {
+                if (sr == 0)
+                {
+                    sr = sizeRes;
+
+                }
+
+                if (sr == sizeRes)
+                {
+                    Matrix.path = @"C:\Work\For D\For D\Resampling\" + Path.GetFileNameWithoutExtension(file) + ".bin";
+                    //File.Create(Matrix.path) ;
+                    //FileStream fs = new FileStream(Matrix.path, FileMode.Append);
+                    //  BinaryWriter sw = new BinaryWriter( fs);
+                }
+                Matrix matrix = Matrix.Reading(file);
+                var ph = Matrix.MatrixSeparation(matrix);
+                var ph1 = ph.x;
+                var ph2 = ph.y;
+                var ph3 = ph.z;
+                var df = Matrix.DemodulRefl(ph1, ph2, ph3);
+                var bf = Matrix.ButterworthFilter(df);
+                Matrix.Resampling(bf);
+                sr--;
+            }
         }
+        //Application.EnableVisualStyles();
+        //Application.SetCompatibleTextRenderingDefault(false);
+        //Application.Run(new Form1());
     }
-}
+    }
+
--- For D/Resampling.cs
+++ For D/Resampling.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,27 @@ namespace For_D
     public partial class Matrix
     {
       public static  Matrix resBuffer;
+        public static BinaryWriter sw;
+        public static FileStream fs;
+        public static string path;
         
-        public static Matrix Resampling(Matrix data)
-        { int z = 0;
+        public static void Resampling(Matrix data)
+        {
+            BinaryWriter sw = new BinaryWriter(new FileStream(path,FileMode.Append));
+            for (int j = 0; j < data.N; j++)
+                {//int z = 0;
             for (int i = 0; i < data.M-10; i+=10)
             {
-                for (int j = 0; j < data.N; j++)
-                {
-                    resBuffer[z, j] = (data[i, j] + data[i + 1, j] + data[i + 2, j] + data[i + 3, j] +
-                        data[i + 4, j] + data[i + 5, j] + data[i + 6, j] + data[i + 7, j] + data[i + 8, j] + data[i + 9, j]) / 10;
-
-                }
-                z++;
+                
+                   // resBuffer[z, j
+                       sw.Write ((data[i, j] + data[i + 1, j] + data[i + 2, j] + data[i + 3, j] +
+                        data[i + 4, j] + data[i + 5, j] + data[i + 6, j] + data[i + 7, j] + data[i + 8, j] + data[i + 9, j]) / 10);
+               // z++;
             }
-            return resBuffer;
+                
+            }
+            //return resBuffer;
+            sw.Close();
         }
     }
 }
